97547be3ea5fb59ade0a0b4f2cb1061ba3f919d8
Josh <josh@civo.com>
2024-06-03 18:10:42 +0100

    fixes to setup script

--- script.sh
+++ script.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
+set -e
+
+# Ensure the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-# install all the docker dependencies
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install all the Docker dependencies
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# stop docker
+# Stop Docker
 sudo systemctl stop docker
 
-# modify the docker daemon config to use the cgroupfs cgroup driver
+# Modify the Docker daemon config to use the cgroupfs cgroup driver
 sudo mkdir -p /etc/docker
 
-# do any modifications to the docker daemon config here
-sudo tee /etc/docker/daemon.json <<EOF
+# Do any modifications to the Docker daemon config here
+sudo tee /etc/docker/daemon.json > /dev/null <<-EOF
 {
   "default-runtime": "nvidia",
   "runtimes": {
@@ -37,11 +46,14 @@ sudo tee /etc/docker/daemon.json <<EOF
 }
 EOF
 
-# start docker again
+# Start Docker again
 sudo systemctl start docker
 
-# install python 3.11
-sudo apt-get install python3.11 python3.11-dev python3.11-venv -y
+# Install Python 3.11 and its dependencies
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install -y python3.11 python3.11-dev python3.11-venv
 
-# disable automatic updates
-sudo apt remove unattended-upgrades
+# Disable automatic updates
+sudo apt-get remove -y unattended-upgrades
